c29a2e16dcde8dcc9ada02355ab1658f79796173
Migu37 <miguel.wolf22@gmail.com>
2015-05-23 11:26:55 -0300

    wq

--- Sebastiao/src/Sebastiao/TelaInicial.java
+++ Sebastiao/src/Sebastiao/TelaInicial.java
@@ -26,7 +26,7 @@ public class TelaInicial extends JFrame {
 
 	private JPanel contentPane;
 	/**
-	 * @author Miguel Wolfgrann
+	 * @author Miguel Wolfgrann, Narciso , Renan e Hermilaiane
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
